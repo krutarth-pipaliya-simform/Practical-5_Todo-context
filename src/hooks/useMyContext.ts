44ea49e9f039c650303c211d28175d8cf1430492
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-29 14:52:29 +0530

    fix: use undefined in union type

--- src/hooks/useMyContext.ts
+++ src/hooks/useMyContext.ts
@@ -1,6 +1,6 @@
 import { useContext, type Context } from "react";
 
-export const useMyContext = <T>(Context: Context<T>) => {
+export const useMyContext = <T>(Context: Context<T> | undefined) => {
     const data = useContext(Context);
     if (!data) throw new Error("Use Context inside wrapper");
     return data;
