edec82855b7f88c97274ca80cbb3a045af4cca9a
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-15 16:44:11 +0530

    feat: add dark mode functionality.

--- src/components/form/TaskForm.tsx
+++ src/components/form/TaskForm.tsx
@@ -1,15 +1,26 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { Button } from "../ui/button";
 import { FormField } from "./FormField";
 import { useTodoContext } from "@/hooks/useTodoContext";
 
 export const TaskForm = memo(() => {
     const { createTodo } = useTodoContext();
+    const [mode, setMode] = useState(() => document.body.classList.contains("dark"));
 
     return (
-        <header className="p-4">
+        <header className="p-4 flex gap-4">
+            <Button
+                className=""
+                variant="outline"
+                onClick={() => {
+                    document.body.classList.toggle("dark");
+                    setMode(!mode);
+                }}
+            >
+                {mode ? "Dark" : "Light"} Mode
+            </Button>
             <form
-                className="flex gap-4"
+                className="flex-1 flex gap-4"
                 action={(formData: FormData) => {
                     if (String(formData.get("todo")).trim() === "") {
                         alert("please enter some text");
