1685a3467fa626218472851c8f258e58e618f4e0
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-22 19:04:12 +0530

    refact: remove provider component, use directly.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import { TaskForm } from "@/components/form/TaskForm";
 import { TodoWrapper } from "@/components/TodoWrapper";
-import { TodoProvider } from "@/store/TodoProvider";
 
 export const Home = () => {
     return (
-        <TodoProvider>
+        <>
             <TaskForm />
             <TodoWrapper />
-        </TodoProvider>
+        </>
     );
 };
