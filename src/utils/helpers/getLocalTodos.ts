4d6f38f2b908c256f83bb1486da3b18f864e8a08
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-22 15:41:01 +0530

    fix: use isArray

--- src/utils/helpers/getLocalTodos.ts
+++ src/utils/helpers/getLocalTodos.ts
@@ -10,7 +10,7 @@ export function getLocalstorageTodos() {
 
     const parsedTodos = JSON.parse(todos ?? "");
 
-    if (!(parsedTodos instanceof Array)) {
+    if (!Array.isArray(Array)) {
         throw new Error("Localstorage has tempered data");
     }
     return validTodos(parsedTodos);
